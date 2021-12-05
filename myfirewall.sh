e802514d4eb907e8263dac4cf85b0e781da554bf
Antonios Atlasis <34843366+aatlasis@users.noreply.github.com>
2021-12-05 13:36:02 +0100

    Add files via upload

--- myfirewall.sh
+++ myfirewall.sh
@@ -7,6 +7,8 @@ do
 	protocol=""
 	dport=""
 	sport="" 
+	icmptype=""
+	icmpcode=""
 	logarray=($line)
 	#echo "${logarray[19]}"
 	if  [[ ${logarray[13]} == PROTO=* ]] ;
@@ -19,6 +21,9 @@ do
 	elif  [[ ${logarray[14]} == SPT=* ]] ;
 	then
 		sport="${logarray[14]}"
+	elif  [[ ${logarray[14]} == TYPE=* ]] ;
+	then
+		icmptype="${logarray[14]}"
 	fi
 	if  [[ ${logarray[15]} == PROTO=* ]] ;
 	then
@@ -29,6 +34,12 @@ do
 	elif  [[ ${logarray[15]} == DPT=* ]] ;
 	then
 		dport="${logarray[15]}"
+	elif  [[ ${logarray[15]} == TYPE=* ]] ;
+	then
+		icmptype="${logarray[15]}"
+	elif  [[ ${logarray[15]} == CODE=* ]] ;
+	then
+		icmpcode="${logarray[15]}"
 	fi
 	if  [[ ${logarray[16]} == SPT=* ]] ;
 	then
@@ -36,14 +47,30 @@ do
 	elif  [[ ${logarray[16]} == DPT=* ]] ;
 	then
 		dport="${logarray[16]}"
+	elif  [[ ${logarray[16]} == TYPE=* ]] ;
+	then
+		icmptype="${logarray[16]}"
+	elif  [[ ${logarray[16]} == CODE=* ]] ;
+	then
+		icmpcode="${logarray[16]}"
 	fi
 	if  [[ ${logarray[17]} == DPT=* ]] ;
 	then
 		dport="${logarray[17]}"
+	elif  [[ ${logarray[17]} == TYPE=* ]] ;
+	then
+		icmptype="${logarray[17]}"
+	elif  [[ ${logarray[17]} == CODE=* ]] ;
+	then
+		icmpcode="${logarray[17]}"
 	fi
 	if [[ $protocol == "PROTO=2" ]] ; then
 		protocol="PROTO=IGMP"
 	fi
-	echo "${logarray[0]} ${logarray[6]} ${logarray[7]} ${logarray[8]} $protocol $sport $dport"
+	if [[ $protocol == "PROTO=ICMP" ]] ; then
+		echo "${logarray[0]} ${logarray[6]} ${logarray[7]} ${logarray[8]} $protocol $icmptype $icmpcode"
+	else
+		echo "${logarray[0]} ${logarray[6]} ${logarray[7]} ${logarray[8]} $protocol $sport $dport"
+	fi
 	#echo "$line"
 done < "$input"
